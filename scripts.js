b4dd6690515f8ed4366000806619cb71b07b8542
erkondex123 <113276415+erkondex123@users.noreply.github.com>
2025-04-04 03:48:37 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ async function sendMessage() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Bearer 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` // Используем API-ключ
+                "Authorization": `Bearer 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` // Используем API-ключ
             },
             body: JSON.stringify({
                 model: "gpt-3.5-turbo",
